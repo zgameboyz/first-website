b107f729b8f4715a4b846eed08d132c0addbb004
Joel Connell <joel.connell@yahoo.com>
2021-04-21 17:02:08 -0700

    added prompts to website

---
+++ javascript.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let food = prompt("What is your favorite food?");
+
+if (food.toLocaleLowerCase() == "burrito"){
+    document.write("Good choice welcome to the club.")
+
+}else if (food.toLocaleLowerCase() == "hamburgers"){
+    document.write("This is a wonderful choise you are an amazing person!")
+}else{
+    document.write("Get off my website >:(")
+}
+
+confirm("Welcome to my website but only if you like burritos or hamburgers.");
+
+
+let color = prompt("What is your favorite color?");
