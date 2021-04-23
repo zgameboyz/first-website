9bdba06f20f8beb99304ca0da9886e6863f7571b
Joel Connell <joel.connell@yahoo.com>
2021-04-23 11:46:05 -0700

    added prompt to ask how many burritos then show that many burritos

--- javascript.js
+++ javascript.js
@@ -1,17 +1,60 @@
 'use strict'
 
-let food = prompt("What is your favorite food?");
 
-if (food.toLocaleLowerCase() == "burrito"){
-    document.write("Good choice welcome to the club.")
-
-}else if (food.toLocaleLowerCase() == "hamburgers"){
-    document.write("This is a wonderful choise you are an amazing person!")
-}else{
-    document.write("Get off my website >:(")
+function websitePrompts() {
+    confirm("Welcome to my website but only if you like burritos or hamburgers.");
+    let food = prompt("What is your favorite food?");
+    let lettersOnlyAnswer = lettersOnly(food);
+    while (lettersOnlyAnswer == false) {
+   
+    
+    food = prompt("What is your favorite food?");
+    lettersOnlyAnswer = lettersOnly(food);
+    
+    }
+    return food;
 }
 
-confirm("Welcome to my website but only if you like burritos or hamburgers.");
 
 
-let color = prompt("What is your favorite color?");
+
+function lettersOnly(answer) {
+    let letters = /^[A-Za-z]+$/;
+    if (answer.match(letters)) {
+        return true;
+    }
+
+    else{
+        return false;
+    }
+    
+
+    }
+
+
+function confirmation(answer) {
+
+        if (answer.toLowerCase() == "burrito") {
+            document.write("Good choice welcome to the club.")
+
+        } else if (answer.toLowerCase() == "hamburgers") {
+            document.write("This is a wonderful choise you are an amazing person!")
+        } else {
+            document.write("Get off my website >:(")
+        }
+        
+    
+    
+    }
+    function numOfBurritos(){
+       let burritoReply = prompt("how many burritos do you want?")
+       
+
+       let src ="burrito.jpg"
+
+       for (let i =0; i < burritoReply; i++) {
+
+       document.write('<img class="burritoPic" src="' + src + '" >');
+
+    }
+}
